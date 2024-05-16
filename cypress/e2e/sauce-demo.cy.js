530ff7f082b37057aeb3a4a494cb64d25bc9622f
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-05-16 13:57:18 +0300

    added 7 new test casses

--- cypress/e2e/sauce-demo.cy.js
+++ cypress/e2e/sauce-demo.cy.js
@@ -33,7 +33,7 @@ describe('SauceDemo', () => {
         cy.url().should('eq',"https://automationexercise.com/account_created")
       });
     
-      it('[3] logging in with wrong pswd ', () => {
+      it('[2] logging in with wrong pswd ', () => {
         cy.visit("https://automationexercise.com/login")
         cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
         cy.get('[data-qa="login-password"]').type("12346)")
@@ -47,22 +47,100 @@ describe('SauceDemo', () => {
         cy.get('[data-qa="login-button"]').click()
         cy.get('.login-form > form > p').should("contain.text","Your email or password is incorrect!")  
       });
-      it('[3] logging with correct data ', () => {
+      it('[4] logging with correct data ', () => {
         cy.visit("https://automationexercise.com/login")
         cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
         cy.get('[data-qa="login-password"]').type("123456")
         cy.get('[data-qa="login-button"]').click()
+        cy.url().should('eq',"https://automationexercise.com/")
       
       });
-      it.only('[3] signing up with no name ', () => {
+      it('[5] signing up with a preused email', () => {
         cy.visit("https://automationexercise.com/login")
         cy.get('[data-qa="signup-name"]').type("abdelrahman")
         cy.get('[data-qa="signup-email"]').type("abdelrahmanbasheer72@gmail.com")
         cy.get('[data-qa="signup-button"]').click()
         cy.get('.signup-form > form > p').should("contain.text","Email Address already exist!") 
       });
+      it('[6] filtering women products', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="#Women"]').click()
+        cy.get('a[href="/category_products/1"]').click()
+        cy.url().should('eq',"https://automationexercise.com/category_products/1")
+        //could add assertion for dress name
+      });
+      it('[7] filtering men products', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="#Men"]').click()
+        cy.get('a[href="/category_products/3"]').click()
+        cy.url().should('eq',"https://automationexercise.com/category_products/3")
+        //could add assertion for dress name
+      });
+      it('[8] filtering kids products', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="#Kids"]').click()
+        cy.get('a[href="/category_products/4"]').click()
+        cy.url().should('eq',"https://automationexercise.com/category_products/4")
+        //could add assertion for dress name
+      });
+      it('[9] filtering polo products', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="/brand_products/Polo"]').click()
+        cy.url().should('eq',"https://automationexercise.com/brand_products/Polo")
+        cy.get('.product-information').should("contain.text","Blue Top")
+        //could add assertion for dress name
+      });
+      it('[10] clicking on a product and adding it to cart', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="/product_details/1"]').click()
+        cy.url().should('eq',"https://automationexercise.com/product_details/1")
+        cy.get('.product-information').should("contain.text","Blue Top")
+        cy.get(':nth-child(5) > .btn').click();
+        cy.get(".modal-content").should("be.exist")
+        cy.get(".modal-footer > .btn").click();
+        //could add assertion for dress name
+      });
+      it.only('[11] clicking on a product and adding it to cart and checking it in cart and removing it', () => {
+        cy.visit("https://automationexercise.com/login")
+        cy.get('[data-qa="login-email"]').type("abdelrahmanbasheer72@gmail.com")
+        cy.get('[data-qa="login-password"]').type("123456")
+        cy.get('[data-qa="login-button"]').click()
+        // cy.url().should('eq',"https://automationexercise.com/")
+        cy.get('a[href="/product_details/1"]').click()
+        cy.url().should('eq',"https://automationexercise.com/product_details/1")
+        cy.get('.product-information').should("contain.text","Blue Top")
+        cy.get(':nth-child(5) > .btn').click();
+        cy.get(".modal-content").should("be.exist")
+        cy.get(".modal-footer > .btn").click();
+        cy.get('a[href="/view_cart"]').eq(0).click();
+        cy.url().should('eq',"https://automationexercise.com/view_cart")
+        cy.get("#product-1").should("contain.text","Blue Top")
+        cy.get('#product-1 .cart_quantity_delete').click()
+        cy.get("#empty_cart").should("be.exist");
+        //could add assertion for dress name
+      });
+
      
-hh
 
       
 });
